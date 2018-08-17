bba56c29ddb904841d975c6abd8ab232af0579a3
Keith Dahlby <keith@newbo.co>
2018-08-17 09:31:04 -0500

    Pivot to correct row/column understanding

--- __tests__/lib/transforms/border.test.js
+++ __tests__/lib/transforms/border.test.js
@@ -8,11 +8,10 @@ describe('transforms/border', () => {
       height: 4,
       width: 5,
       img: new Buffer([
-        1, 2, 3, 4, // col 0
-        5, 6, 7, 8, // col 1
-        9, 1, 2, 3, // col 2
-        5, 6, 7, 8, // col 3
-        9, 1, 2, 3, // col 4
+        1, 2, 3, 4, 7, // row 0
+        5, 6, 7, 8, 3, // row 1
+        9, 1, 2, 3, 1, // row 2
+        5, 6, 7, 8, 4, // row 3
       ]),
     };
 
@@ -20,11 +19,10 @@ describe('transforms/border', () => {
 
     expect(bmp.img)
       .toEqual(new Buffer([
-        0, 0, 0, 0, // col 0
-        0, 6, 7, 0, // col 1
-        0, 1, 2, 0, // col 2
-        0, 6, 7, 0, // col 3
-        0, 0, 0, 0, // col 4
+        0, 0, 0, 0, 0, // row 0
+        0, 6, 7, 8, 0, // row 1
+        0, 1, 2, 3, 0, // row 2
+        0, 0, 0, 0, 0, // row 3
       ]));
   });
 });
